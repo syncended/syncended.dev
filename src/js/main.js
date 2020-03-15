edc60ad4c5db45b3a9d92f0acb32e7858850207a
syncended <syncended@gmail.com>
2020-03-15 19:12:40 +0300

    added new text

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var text = "Welcome to syncended's page. You can find my git repository on follow link: https://github.com/syncended";
+var text = "Welcome to syncended's page. You can find my git profile on follow link: https://github.com/syncended If you want, you can look at my projects here.";
 var typeDuration = 50;
 var cursorDuration = 500;
 
@@ -37,8 +37,15 @@ function cursorAnimation() {
 function formatText(txt) {
 	var out = "";
 	for(var i = 0; i < txt.length; i++) {
+		if(i == 72 || i == 101) {
+			out += '<br>';
+		}
 		if(i >= 11 && i < 23) {
 			out += '<span class="red">' + txt[i] + '</span>'
+		} else if((i > 72 && i < 101) || (i > 44 && i < 48)) {
+			out += '<a href="https://github.com/syncended">' + txt[i] + "</a>";
+		} else if(i > 142 && i < 147) {
+			out += '<a href="projects.html">' + txt[i] + "</a>";
 		} else {
 			out += txt[i];
 		}
