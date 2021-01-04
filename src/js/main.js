98997489d74b13469cdb505c4fb0ee0116e4bd1f
syncended <syncended@gmail.com>
2021-01-04 11:19:49 +0300

    add link on link

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,12 @@ var typeDuration = 50;
 var cursorDuration = 500;
 var logoDuration = 5;
 
-var wrapperRules = [["s", "red", 12, 22], ["b", "", 32], ["a", "https://github.com/syncended", 48, 51]];
+var wrapperRules = [
+	["s", "red", 12, 22], 
+	["b", "", 32], 
+	["a", "https://github.com/syncended", 49, 51], 
+	["a", "https://github.com/syncended", 76, 104]
+];
 
 function logoAnimation() {
 	var pos = 0;
